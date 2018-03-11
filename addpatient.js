4791d01ec5bedb216ccab454345e075746649217
cwnil59 <cwnil59@gmail.com>
2018-03-11 12:45:01 +0000

    added database table at the bottom of the form
    form still doesn't submit, because javascript is broken

---
+++ addpatient.js
@@ -0,0 +1,26 @@
+function addpatient() {
+var nhsid = document.getElementById("nhsid").value;
+var forename = document.getElementById("forename").value;
+var lastname = document.getElementById("lastname").value;
+var ad1 = document.getElementById("ad1").value;
+var ad2 = document.getElementById("ad2").value;
+//var dob = document.getElementById("dob").value;
+//var gender = document.getElementById("gender").value;
+
+document.getElementById("results").innerHTML = "Form submitted. Patient added to database.";
+
+var table = document.getElementById("fulldata").value;
+
+var row2 = table.insertRow();
+row2.insertCell().innerHTML = nhsid;
+row2.insertCell().innerHTML = forename;
+row2.insertCell().innerHTML = lastname;
+row2.insertCell().innerHTML = "nil";
+row2.insertCell().innerHTML = "nil";
+row2.insertCell().innerHTML = ad1;
+row2.insertCell().innerHTML = ad2;
+row2.insertCell().innerHTML = "nil";
+
+
+
+}
